80764824a2304780e80732f86e0acef0e7f0a3c0
olesya <ochernova956@gmail.com>
2026-01-17 03:41:48 +0700

    Update Clock.js

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Clock() {
+function Clock() {
+
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        setInterval(() => setTime(new Date()), 1000)
+    }, [])
+
   return (
     <clock>
         <div>
-            <div className='time'>12:47</div>
-            <div className='date'>Saturday, January 17</div>
+            <div className='time'>{time.getHours()}:{time.getMinutes()}</div>
+            <div className='date'>{getWeekDay(time.getDay())}, {getMonthName(time.getMonth())} {time.getDate()}</div>
         </div>
         <div className='quote'>Be the flame, not the moth</div>
     </clock>
   )
 }
+
+function getWeekDay (day){
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    return weekday[day];
+}
+
+function getMonthName (month){
+    const year = ["January","February","March","April","May","June","Juky", "August", "September", "October", "November", "December"];
+    return year[month];
+}
+
+export default Clock;
