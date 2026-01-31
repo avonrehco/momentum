75296b9dec2b18b0b1868b0d015c638a67443dd1
olesya <ochernova956@gmail.com>
2026-01-31 12:31:32 +0700

    commit
    add function to change the time format

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,7 +3,11 @@ import { GoKebabHorizontal } from "react-icons/go";
 
 function Clock() {
 
+    let [menuOpen, setMenuOpen] = useState(false)
+
     const [time, setTime] = useState(new Date())
+
+    let [toggled, setToggled] = useState(false)
 
     useEffect(() => {
         setInterval(() => setTime(new Date()), 1000)
@@ -12,12 +16,33 @@ function Clock() {
   return (
     <clock>
         <div className='time'>
-            <div className='time-clock'>{time.getHours()}:{getRightMinutes(time.getMinutes())}</div>
-            <GoKebabHorizontal className='time-menu'/>
+            <div className='time-clock'>{getTimeFormat(time.getHours(), toggled)}:{getRightMinutes(time.getMinutes())}</div>
+            <GoKebabHorizontal onClick  = {() => setMenuOpen(menuOpen = !menuOpen)} className={`time-menu ${menuOpen && "active"}`} />
+
+            {menuOpen && (
+            <div className='clock-menu'>
+                <div className='12-clock'>12-hour clock</div>
+                <label className = 'switch'>
+                    <input type='checkbox' checked = {toggled} onChange = {() => setToggled(!toggled)}/>
+                    <span className='slider'></span>
+                </label>
+            </div>
+        )}
+
         </div>
         <div className='date'>{getWeekDay(time.getDay())}, {getMonthName(time.getMonth())} {time.getDate()}</div>
     </clock>
   )
+}
+
+function getTimeFormat (hour, toggled) {
+    if (toggled){
+        if (hour > 12) return hour - 12;
+        else return hour
+    } else {
+        if (hour < 10) return '0' + hour; 
+        else return hour
+    }
 }
 
 function getWeekDay (day){
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdOutlineCheckBox, MdOutlineImage } from "react-icons/md";
 
 export default function Footer() {
+
+  let [galleryOpen, setGalleryOpen] = useState(false)
+
   return (
     <footer>
         <div className='change-bg'>
-            <MdOutlineImage className='footer-icon'/>
+            <MdOutlineImage onClick = {() => setGalleryOpen(galleryOpen = !galleryOpen)} className='footer-icon'/>
+
+            {galleryOpen && (
+              <div className='gallery'></div>
+            )}
+
         </div>
         <div className='quote'>Be the flame, not the moth</div>
         <div className='tasks'>
